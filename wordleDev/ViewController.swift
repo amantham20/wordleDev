acea3faa1d9ad95c4debdd399f39f8d613d8c9b9
Aman Dhruva Thamminana <48414198+amantham20@users.noreply.github.com>
2023-01-16 21:28:21 -0500

    Initial Commit

---
+++ wordleDev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wordleDev
+//
+//  Created by Aman Dhruva Thamminana on 1/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
